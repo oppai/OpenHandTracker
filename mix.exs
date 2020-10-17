d4f00fe69309236111f5de9c09828d4d7dd5af91
oppai <hotsoup.h@gmail.com>
2020-10-18 04:23:03 +0900

    Add cachex

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule OpenHandTracker.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:cachex, "~> 3.3"}
     ]
   end
 
